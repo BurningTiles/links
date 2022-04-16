9651699d6e75f8d2034ecd7d9a0cc9f3a82683c9
Manish Kumawat <30183799+BurningTiles@users.noreply.github.com>
2022-04-16 13:31:24 +0530

    Link updated

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-window.location.href = "https://skinny-ginger-66f.notion.site/Useful-Links-409ca82e7f094cc08b4b80a3094577a9"
+window.location.href = "https://skinny-ginger-66f.notion.site/8f9225885fba45cea861e9239bbd553d?v=4b068ab5c4ff499b87f45d4644888ea4"
